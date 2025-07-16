8aa65b462e8bb8e30cca740bc869bd386414ae69
John Leon <leon3847w@gmail.com>
2025-07-15 21:16:01 -0500

    Exercises with strings

--- my_python_practice.py
+++ my_python_practice.py
@@ -1,2 +1,25 @@
 '''This file is to practice my knowledge by Python'''
 
+company = 'Coding For All'
+print(company)
+print(len(company))
+
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+print(company.strip('Coding '))
+print(company.index('Coding'))
+print(company.replace('All', 'Everyone'))
+print(company.split())
+
+companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+print(companies.split(', '))
+
+print(company[0])
+print(company.rindex('l'))
+
+script = 'You cannot end a sentence with because because because is a conjunction'
+print(script.strip('because '))
