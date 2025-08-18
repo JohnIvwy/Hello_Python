1e829845c252463afb6d0bc5bf508132a5838683
John Leon <leon3847w@gmail.com>
2025-08-18 16:52:03 -0500

    Exceptions Handling (Manejo de errores)

--- my_python_practice.py
+++ my_python_practice.py
@@ -1,58 +1,43 @@
 '''This file is to practice my knowledge by Python'''
 
-### Classes ###
+### Exceptions Handling (Manejo de errores) ###
 # Meaning: 
 
 # To declarare:
-class Person: # nombres de clases en Camelcase UnEjemploEsEste
-    # __init__ constructor de la clase, permite dar valores al objeto al crearlo
-    # 'self' le asigna esos valores al objeto creado y permite acceder a ellos dentro de la clase
-    def __init__(self, name, age, height, gender): 
-        # atributos aquí abajo y se pasan por parámetros
-        self.name = name
-        self.age = age
-        self.height = height
-        self.gender = gender
+try:
+    pass # Ejecuta el código
+except:
+    pass # Se ejecuta si manda error
+else: # Opcional
+    pass # Se ejecuta si no manda error
+finally: # Opcional
+    pass # Se ejecuta siempre, con error o sin error
 
-    # definimos sus métodos
-    def greeting(self):
-        print(f'¡Hola {self.name}!')
-    
-    def get_age(self):
-        return self.age
-    
-    def prt_full_name(self):
-        self.name = 'Raton'
-        self.age = 32
-    
-    def prt_attributes(self):
-        print(f'Name: {self.name}')
-        print(f'Age: {self.age}')
-        print(f'Height: {self.height}')
-        print(f'Gender: {self.gender}')
-    
-# Instanciamos el objeto (lo creamos)
-john = Person('John', 21, 1.70, 'Male')
 
-# Accedemos a sus atributos y métodos si son públicos
-john.greeting()
-print(john.get_age())
+# Excepciones por tipo
 
-# Child class
-class Stutdent(Person):
-    def __init__(self, name, age, height, gender):
-        super().__init__(name, age, height, gender)
-    
-    def prt_full_name(self):
-        super().prt_full_name()
-        self.age = 64
+try:
+    pass # Ejecuta el código
+except ValueError:
+    pass # Se ejecuta cuando se produce un ValueError
+except TypeError:
+    pass # Se ejecuta cuando se produce un TypeError
+except:
+    pass # Se ejecuta si no se cumplen los otros except
 
-pedro = Stutdent('pedro', 12, 1.32, 'Male')
-pedro.prt_full_name()
-pedro.prt_attributes()
+
+# Captura de la información de la excepción
+
+try:
+    pass # Ejecuta el código
+except ValueError as e:
+    print(e) # Encuentra un ValueError y lo guarda en una variable (e = error)
+except Exception as my_random_error_name:
+    print(my_random_error_name)
+    # Captura cualquier error y lo guarda en una variable
+
 
 # ---------------------------------------
 # -------------- Exercises --------------
 # ---------------------------------------
-# 1-) 
-
+# 1-)
