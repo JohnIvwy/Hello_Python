e8eceb6f1c44a8ce5830929308930d4cd853dfb6
John Leon <leon3847w@gmail.com>
2025-12-16 21:39:53 -0500

    Resolving Level 2

--- Basic/remenbering_fundamentals.py
+++ Basic/remenbering_fundamentals.py
@@ -1,5 +1,9 @@
 ''' This is my file for practicing my knowledge in the fundamental course.'''
 
+# ---------------------------------------------------------------------------
+# ---------------------------- LEVEL ONE ------------------------------------
+# ---------------------------------------------------------------------------
+
 name = 'John Leon'
 age = 22
 language = 'Python'
@@ -64,3 +68,35 @@ try:
     print('2'*2)
 except:
     print('Fatal error')
+    
+
+
+# ---------------------------------------------------------------------------
+# ---------------------------- LEVEL TWO ------------------------------------
+# ---------------------------------------------------------------------------
+
+lista = [1,2,3,4,5]
+for value in lista:
+    print(value)
+
+print(sum(lista))
+
+frutas = ['Apple', 'Pineapple', 'Orange', 'Strawberry', 'coconut']
+print(len(frutas))
+
+for fruta in frutas:
+    print(fruta.upper())
+
+print(min(lista))
+print(max(lista))
+
+list_reps = [1,2,3,9,3,2,1,8,3,1,2,4,7,8,6,4,5,4]
+st = set(list_reps)
+print(st)
+
+diccionario = {'nombre': 'John', 'edad': 22, 'ciudad':'La Calera'}
+print(diccionario.items())
+
+for i in st:
+    if (i % 2 == 0):
+        print(i)
