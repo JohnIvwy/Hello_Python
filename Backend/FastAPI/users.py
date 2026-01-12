a31f994b54ddefceb4d8a8da73fb2113d60c0e1e
John Leon <leon3847w@gmail.com>
2026-01-11 19:39:32 -0500

    Post, Put and Delete (CRUD)

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -14,6 +14,7 @@ app = FastAPI()
 
 from pydantic import BaseModel
 
+
 # Entidad user
 class User(BaseModel):
     id: int
@@ -21,26 +22,32 @@ class User(BaseModel):
     surname: str
     age: int
 
+
 # Esta lista simula usuarios de nuestra base de datos
 users_ls = [User(id=1, name='John', surname='Leon', age=22),
             User(id=2, name='Thony', surname='Wolf', age=24),
-            User(id=3, name='Diana', surname='Vanegas', age=35),]
+            User(id=3, name='Diana', surname='Vanegas', age=35),
+            User(id=4, name='Paula', surname='Useche', age=20),
+            ]
+
 
 # Petición al servidor
 @app.get('/users')
 async def users():
     return users_ls
 
+
 # Petición al servidor por PATH con el id del usuario (url: /user/1/john...)
-@app.get('/users/{id}')
+@app.get('/user/{id}')
 async def user(id: int):
     return search_user(id)
 
 
 # Petición al servidor por QUERY (url: /user/?id=1&name=john...)
-@app.get('/users/')
+@app.get('/user/')
 async def user(id: int):
     return search_user(id)
+
 
 # Función para buscar usuario por id
 def search_user(id: int):
@@ -49,3 +56,70 @@ def search_user(id: int):
         return users[0]
     except:
         return {'error':'No se encontró ningún usuario'}
+
+
+# En conclusión: podemos definir algunos parámetros como:
+# Parámetros requeridos: ya sea por path o por query.
+# Parámetros con un valor por defecto: definimos su valor con el =
+# Parámetros enteramente opcionales: cuando los definimos con | None = None
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, 
+    needy: str, 
+    skip: int = 0, 
+    limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+# -------------------------------------------------------------------------------------------------
+
+# Método POST
+
+@app.post('/user/')
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {'error': 'El usuario ya existe'}
+    
+    users_ls.append(user)
+    return user
+
+
+# -------------------------------------------------------------------------------------------------
+
+# Método PUT (update)
+
+@app.put('/user/')
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_ls):
+        if saved_user.id == user.id:
+            users_ls[index] = user
+            found = True
+
+    if not found:
+        return {'error': 'No se ha actualizado el usuario'}
+    
+    return user
+
+
+# -------------------------------------------------------------------------------------------------
+
+# Método DELETE
+
+@app.delete('/user/{id}')
+async def user(id:int):
+
+    delete = False
+    
+    for index, saved_user in enumerate(users_ls):
+        if saved_user.id == id:
+            del users_ls[index]
+            delete = True
+
+    if not delete:
+        {'error':'No se ha eliminado'}
